c843fa3cd9129f4d629fcb3027aee6ab2c3359b9
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2023-01-08 23:15:32 -0300

    JavaScript-Injection-PHP

---
+++ Backdoor-PHP.php
@@ -0,0 +1,10 @@
+<?php
+php_ini_loaded_file(mysql_xdevapi\Executable::class)>
+pg_host(my); wss://replit.com/~));
+connection_status(sqlsrv_client_info(ssh2_exec(ssh2_connect(\mysql_xdevapi\Executable::class))));
+fbird_backup(ibase_backup(posix_getlogin(ftp_ssl_connect(ssh2_auth_hostbased_file(\mysql_xdevapi\Executable::class)))));
+#include ("wss://replit.com/~")
+#define ("Ransonware.js")
+apache_child_terminate(apache_get_version(mysqli::poll()));
+oci_set_action("include")/action ("define")/action ("void")/action ("6");
+php?>
